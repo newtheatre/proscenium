4c860926604c160ddf7bf835522da43ec3ed6463
Matt Adcock <matthew.n.adcock@gmail.com>
2026-01-13 19:57:15 +0000

    chore: restructure hub configuration in nuxt.config.ts to seperate dev and production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,27 @@ export default defineNuxtConfig({
   ],
 
   $production: {
+    hub: {
+      db: {
+        dialect: 'sqlite',
+        driver: 'd1', // FIXME: https://github.com/nuxt-hub/core/pull/775
+        connection: { databaseId: 'c4200074-3ce8-411d-b428-811277057e6b' },
+      },
+      // KV namespace (binding defaults to 'KV')
+      kv: false,
+      // Cache KV namespace (binding defaults to 'CACHE')
+      cache: {
+        driver: 'cloudflare-kv-binding',
+        namespaceId: 'c6d3be0190414e1ab170a0788ec51fb6',
+      },
+      // R2 bucket (binding defaults to 'BLOB')
+      blob: {
+        driver: 'cloudflare-r2',
+        bucketName: '<bucket-name>', // TODO: replace once we have a bucket
+        binding: 'BLOB',
+      },
+    },
+
     image: {
       provider: 'cloudflare',
       cloudflare: {
@@ -41,24 +62,10 @@ export default defineNuxtConfig({
   },
 
   hub: {
-    db: {
-      dialect: 'sqlite',
-      driver: 'libsql', // FIXME: https://github.com/nuxt-hub/core/pull/775
-      connection: { databaseId: 'c4200074-3ce8-411d-b428-811277057e6b' },
-    },
-    // KV namespace (binding defaults to 'KV')
+    db: 'sqlite',
     kv: false,
-    // Cache KV namespace (binding defaults to 'CACHE')
-    cache: {
-      driver: 'cloudflare-kv-binding',
-      namespaceId: 'c6d3be0190414e1ab170a0788ec51fb6',
-    },
-    // R2 bucket (binding defaults to 'BLOB')
-    blob: {
-      driver: 'cloudflare-r2',
-      bucketName: '<bucket-name>', // TODO: replace once we have a bucket
-      binding: 'BLOB',
-    },
+    cache: false,
+    blob: true,
   },
 
   eslint: {
